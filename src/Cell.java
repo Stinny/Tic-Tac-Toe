10b3bc7c88b4676c01f3960a691ac2e595ae7a5a
wongm1888 <marcuswong128@gmail.com>
2018-11-13 16:18:54 -0500

    Cell class representing each space of a tic-tac-toe board. Can select its enumeration via CELLName.setCircle(), CELLName.setCross(), and CELLName.setEmpty()

--- src/Cell.java
+++ src/Cell.java
@@ -2,17 +2,24 @@
 public class Cell {
 	private int row;
 	private int col;
-	
+	Status content; 
 	
 	public Cell(int row, int col) {
 		this.row = row;
 		this.col = col;
 	}
 	
-	public getStatus {
-		
+	public void setCircle() {
+		this.content = Status.CIRCLE;
 	}
 	
+	public void setCross() {
+		this.content = Status.CROSS;
+	}
+	
+	public void setEmpty() {
+		this.content = Status.EMPTY;
+	}
 	
 	
 }
