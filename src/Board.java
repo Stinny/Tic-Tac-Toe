c699a7154e894a1e5cc6dde1f112ffe5f19fd306
wongm1888 <marcuswong128@gmail.com>
2018-11-13 18:16:24 -0500

    implementation of checking win cases

--- src/Board.java
+++ src/Board.java
@@ -6,29 +6,84 @@ public class Board {
 	public Board() {
 		for (int row = 0; row < maxRows; row++){
 			for (int col = 0; col < maxCol; col++) {
-				board[row][col] = null;
+				board[row][col] = new Cell(row, col);
 			}
 		}
 	}
 	
-	private void setCircle(Cell[][] board) {
+	private int checkDiagonalCase() {
+		int victory = 0;
+		if (board[1][1].content == Status.CIRCLE) {
+			if ((board[0][0].content == Status.CIRCLE && board[2][2].content == Status.CIRCLE) //if diagonal from [0][0] to [2][2]
+					|| (board[0][2].content == Status.CIRCLE && board[2][0].content == Status.CIRCLE)) {// or diagonal from [0][2] to [2][0]
+				victory = 1;
+			}
+		}
+		else if (board[1][1].content == Status.CROSS) {
+			if ((board[0][0].content == Status.CROSS && board[2][2].content == Status.CROSS) //if diagonal from [0][0] to [2][2]
+					|| (board[0][2].content == Status.CROSS && board[2][0].content == Status.CROSS)) {// or diagonal from [0][2] to [2][0]
+				victory = -1;
+			}
+		}
 		
+		return victory;
 	}
 	
-	private int checkStatus() {
-		//check horizontal 
-		int row, col = 0;
+	private int checkHorizontalCase() {
 		
-		for(row = 0; row < maxRows; row++){
-			for (col = 0; col < maxCol; col++) {
-				if(board[row][col].content == Status.CIRCLE) {
-				
+		int victory = 0; //if victory is 0, no one has won yet; if 1, circle wins; if -1, cross wins
+		for (int col = 0; col < maxCol; col++) {
+			int row = 0;
+			if (board[row][col].content == Status.CIRCLE) {
+				row++;
+				if(board[row][col].content == Status.CIRCLE){
+					row++;
+					if(board[row][col].content == Status.CIRCLE) {
+						victory = 1;
+					}
 				}
 			}
-			
+			else if (board[row][col].content == Status.CROSS) {
+				row++;
+				if(board[row][col].content == Status.CROSS){
+					row++;
+					if(board[row][col].content == Status.CROSS) {
+						victory = -1;
+					}
+				}
+			}
 		}
-		return 0;
+		return victory;
 	}
+	
+	private int checkVerticalCase() {
+		int victory = 0; //if victory is 0, no one has won yet; if 1, circle wins; if -1, cross wins
+		for (int row = 0; row < maxRows; row++) {
+			int col = 0;
+			if (board[row][col].content == Status.CIRCLE) {
+				col++;
+				if(board[row][col].content == Status.CIRCLE){
+					col++;
+					if(board[row][col].content == Status.CIRCLE) {
+						victory = 1;
+					}
+				}
+			}
+			else if (board[row][col].content == Status.CROSS) {
+				col++;
+				if(board[row][col].content == Status.CROSS){
+					col++;
+					if(board[row][col].content == Status.CROSS) {
+						victory = -1;
+					}
+				}
+			}
+		}
+		return victory;
+	}
+	
+	
+	
 	
 	//print board --- jframe/jpanels 
 	
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class TicTacToe {
+public class TicTacToe extends Application{
+	
+	public Parent createPane() {
+		Pane pane = new Pane();
+		pane.setPrefSize(600, 600);
+		
+		return pane;
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setScene(new Scene(createPane()));
+		primaryStage.show();
+		
+		
+	}
+		
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 
 }
