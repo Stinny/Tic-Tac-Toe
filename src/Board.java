bcdcb321497f298efb00d7c347510adf65f3aff6
wongm1888 <marcuswong128@gmail.com>
2018-11-13 15:34:10 -0500

    updates

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,18 @@
 
 public class Board {
-
+	private int maxRows, maxCol = 3;
+	private Cell[][] board = new Cell[maxRows][maxCol];
+	
+	public Board() {
+		for (int row = 0; row < maxRows; row++){
+			for (int col = 0; col < maxCol; col++) {
+				board[row][col] = null;
+			}
+		}
+	}
+	
+	
+	
+	//print board --- jframe/jpanels 
+	
 }
--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,18 @@
 
 public class Cell {
-
+	private int row;
+	private int col;
+	
+	
+	public Cell(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public getStatus {
+		
+	}
+	
+	
+	
 }
