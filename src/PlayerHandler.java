7d26092502c1ea0d20a28ebe6494cc75d6eff7f5
Lucas Raza <razal@wit.edu>
2018-11-15 17:31:11 -0500

    Updated Server.java, working on TicTacToe Room functionality.

--- src/PlayerHandler.java
+++ src/PlayerHandler.java
@@ -7,15 +7,20 @@ import java.net.Socket;
 public class PlayerHandler {
     private String name;
     private Socket socket;
+    private String password;
 
-    public PlayerHandler(String userName, Socket userSocket)
+    public PlayerHandler(String userName, String userPassword, Socket userSocket)
     {
         this.name = userName;
         this.socket = userSocket;
+        this.password = userPassword;
     }
     public String getUserName(){
         return name;
     }
+
+    public String getPassword(){ return password;}
+
     public Socket getSocke(){
         return socket;
     }
--- src/Server.java
+++ src/Server.java
@@ -1,60 +1,146 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
 
 /**
- * Created by lucasraza on 3/8/17.
+ * Created by lucasraza on 3/8/17
+ *
+ *
  */
 public class Server {
 
-	private static boolean connected = false;
-	private static ArrayList<String> sockets = new ArrayList<String>();
+	private static HashMap<String, PlayerHandler> matching;
 
-	private static HashMap<String, PlayerHandler> playerMap;
+	private static ArrayList<Socket> sockets;
 
-	private static BufferedReader receive;
 
-	private static ArrayList<Thread> threads = new ArrayList<Thread>();
+	public static void main(String[] args){
 
-	public static void main(String[] args) throws java.io.IOException {
+		int PORT = 2000;
+		// Have the server run until it is killed.
+		while(true)
+		{
+			// Monitor connections to PORT (if it is available).
+			try
+			{
+				ServerSocket serverSocket = new ServerSocket(PORT);
+				System.out.println("The server is now running on port " + PORT + "...");
 
-		playerMap = new HashMap<>();
+				Socket connectionSocket = serverSocket.accept();
+				System.out.println("A user has connected from " + connectionSocket.getInetAddress());
 
-		ServerSocket serverSocket = new ServerSocket(2000);
+				BufferedReader clientInput = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-		Socket s;
-		while(!(playerMap.isEmpty())) {
-			if ((s = serverSocket.accept())!=null) {
-				createUser();
+				DataOutputStream serverOutput = new DataOutputStream(connectionSocket.getOutputStream());
+
+				//Gets user's info and connects player
+				matching = connectPlayer(serverSocket);
+
+				//Match players
+				while(!(matching.isEmpty()){
+
+
+
 			}
+
+				//Create Game
+
+				//Store above process as a thread in a HashMap
+
+				//Tic Tac Toe Room Implementation
+
+
+				// Create a server-game based version of TicTacToe.
+//				ServerGame game = new ServerGame(clientInput, serverOutput);
+
+			}
+
+			// Catch any binding/IO errors that may occur.
+			catch(IOException e)
+			{
+				System.err.println(e);
+			}
+	}
+
+
+
+
+
+}
+
+	private static HashMap<String, PlayerHandler> connectPlayer(ServerSocket serverSocket){
+		PlayerHandler player;
+		HashMap<String, PlayerHandler> matching = new HashMap<>();
+			try {
+				Socket s;
+				BufferedReader receive = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				String userName = receive.readLine();
+				String password = receive.readLine();
+				if ((s = serverSocket.accept()) != null) {
+					player = createUser(userName, password,s);
+				}
+			}catch(IOException e){
+				System.out.println(e);
+
+			}
+
+			matching.put(sockets.toString(), player);
+
+
+			return matching;
+	}
+
+
+
+	public static PlayerHandler createUser(String userName, String password, Socket s){
+
+
+			PlayerHandler player = new PlayerHandler(userName,password,s);
+
+			sockets = new ArrayList<>();
+			sockets.add(s);
+
+		    return player;
+
+	}
+
+	public boolean connected(String userName, Socket chatSocket) {
+		if (chatSocket.isConnected()) {
+			System.out.println(userName + " has connected to server on port: " + chatSocket.getLocalPort() + ". IP Address: " + chatSocket.getInetAddress());
+
+			return true;
+		} else {
+			return false;
 		}
 	}
 
-	private static void createUser(){
-		receive = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		String userName = receive.readLine();
-		try {
 
-			if(connected(userName,s)) {
-				sockets.add(s.toString());
 
-				playerMap userHandler = new playerMap(userName, s);
+public class ServerGame{
 
-				playerMap.put(s.toString(), userHandler);
+	public boolean connected = false;
 
-				Thread send = new Thread(new ReceiveThread(s));
-				send.start();
-				threads.add(send);
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+	public ArrayList<String> sockets = new ArrayList<String>();
 
+	private boolean paired = false;
+
+	private BufferedReader input;
+
+	private DataOutputStream output;
+
+	private ArrayList<Thread> threads = new ArrayList<Thread>();
+
+	public ServerGame(BufferedReader clientInput, DataOutputStream serverOutput){
+		input = clientInput;
+		output = serverOutput;
 	}
+}
+
+
+
+
+
 
 	static class ReceiveThread implements Runnable {
 		Socket socket = null;
@@ -69,6 +155,31 @@ public class Server {
 			String message;
 
 			try (BufferedReader r = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+				while ((message = r.readLine()) != null) {
+					if (message.equals(Protocol.MAKE_MOVE)) {
+						String move = in.readLine();
+						String[] l = move.split(" ");
+
+						board.makeMove(Integer.parseInt(l[0]), Integer.parseInt(l[1]), "X");
+						out2.println(Protocol.MOVE_MADE);
+						out2.println(move);
+						switch(board.getStatus()) {
+							case "X":
+								out2.println(Protocol.GAME_LOST);
+								out.println(Protocol.GAME_WON);
+								break;
+							case "O":
+								out2.println(Protocol.GAME_WON);
+								out.println(Protocol.GAME_LOST);
+								break;
+							case "tie":
+								out2.println(Protocol.GAME_TIED);
+								out.println(Protocol.GAME_TIED);
+								break;
+						}
+
+					}
+				}
 				while (true) {
 					message = r.readLine();
 					for (String object : sockets) {
@@ -84,7 +195,7 @@ public class Server {
 		}
 	}
 
-	private static void write(String message, Socket socket, String name) {
+	private void write(String message, Socket socket, String name) {
 		//Why upon closing the PrintWrite does the socket connection close
 		try {
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -98,13 +209,5 @@ public class Server {
 		}
 	}//closes write method
 
-	static public boolean connected(String userName, Socket chatSocket) {
-		if (chatSocket.isConnected()) {
-			System.out.println(userName + " has connected to server on port: " + chatSocket.getLocalPort() + ". IP Address: " + chatSocket.getInetAddress());
 
-			return true;
-		} else {
-			return false;
-		}
-	}
 }
