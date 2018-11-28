dc0c54b4d6a98e34c4fdc93d40449db2ec773630
Lucas Raza <razal@wit.edu>
2018-11-27 20:57:11 -0500

    Update TicTacToe GUI

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -12,6 +12,24 @@ import javafx.stage.Stage;
 
 import javafx.scene.control.Button;
 
+import javafx.scene.image.Image;
+
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+
+import javafx.scene.layout.HBox;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.text.*;
+
+import javafx.scene.layout.VBox;
+
+import javafx.geometry.Pos;
+
+import javafx.scene.layout.Priority;
+
 
 
 public class TicTacToe extends Application{
@@ -22,15 +40,67 @@ public class TicTacToe extends Application{
 
 	private Button exit = new Button("Exit");
 
+	private TextField playerName = new TextField();
+
+	private Text titleArt = new Text();
 
 
-	public void startButton()
+
+	public void startButton(String playerName)
 
 	{
 
-		//information needed from server to complete this method
+		Stage stage = new Stage();
 
-		//will be method that is responsible for connecting game to server
+
+
+		GridPane gridPane = new GridPane();
+
+		gridPane.setAlignment(Pos.CENTER);
+
+		gridPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+		//gridPane.setPadding(new Insets(15));
+
+
+
+		for (int i = 0; i < 3; i++) {
+
+			for (int j = 0; j < 3; j++) {
+
+				Button button = new Button();
+
+				button.setMaxWidth(Double.MAX_VALUE);
+
+				button.setMaxHeight(Double.MAX_VALUE);
+
+				gridPane.add(button, i, j);
+
+				GridPane.setHgrow(button, Priority.ALWAYS);
+
+				GridPane.setVgrow(button, Priority.ALWAYS);
+
+			}
+
+		}
+
+
+
+		Scene scene2 = new Scene(gridPane, 600, 400);
+
+		stage.setTitle(playerName);
+
+		stage.setScene(scene2);
+
+		stage.show();
+
+	}
+
+	public void exitButton()
+
+	{
+
+		System.exit(0);
 
 	}
 
@@ -44,23 +114,55 @@ public class TicTacToe extends Application{
 
 		GridPane g = new GridPane();
 
-		g.setHgap(10);
+		g.setHgap(1);
 
-		g.setVgap(10);
+		g.setVgap(1);
 
 
 
-		g.add(start, 2, 4);
+		//ImageView imv = new ImageView();
 
-		g.add(exit, 2, 5);
+		//Image image = new Image(TicTacToe.class.getResourceAsStream("tictactoe.png"));
 
-		start.setPrefWidth(50);
+		//imv.setImage(image);
 
-		exit.setPrefWidth(50);
+
+
+		//HBox picarea = new HBox();
+
+		//picarea.getChildren().add(imv);
+
+
+
+		//g.add(picarea, 5, 4);
+
+		g.add(start, 200, 200);
+
+		g.add(playerName, 199, 200);
+
+		playerName.setPromptText("Enter player name"); //sets the prompt text for player name text field
+
+
+
+		//g.add(titleArt, 100, 12);
+
+		titleArt.setText("Tic Tac Toe");
+
+		titleArt.setId("fancytext");
+
+		//titleArt.setTextAlignment(TextAlignment.CENTER);
+
+
+
+		start.setPrefWidth(75);
+
+		//exit.setPrefWidth(50);
 
 
 
 		Scene s = new Scene(g, 600, 400);
+
+		s.getStylesheets().add("styles.css");
 
 		primaryStage.setTitle("Tic Tac Toe");
 
@@ -70,6 +172,10 @@ public class TicTacToe extends Application{
 
 
 
+		start.setOnAction(e -> startButton(playerName.getText()));
+
+		exit.setOnAction(e -> exitButton());
+
 	}
 
 
@@ -77,11 +183,3 @@ public class TicTacToe extends Application{
 	public static void main(String[] args) {
 
 		launch(args);
-
-	}
-
-
-
-
-
-}
