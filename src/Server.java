8525380378e52434e95a360451405cf6bb215a90
wongm1888 <marcuswong128@gmail.com>
2018-11-30 18:32:42 -0500

    checkWin

--- src/Server.java
+++ src/Server.java
@@ -64,15 +64,46 @@ public class Server {
         }
 
         public boolean hasWinner() {
-            return
-                    (board[0] != null && board[0] == board[1] && board[0] == board[2])
-                            ||(board[3] != null && board[3] == board[4] && board[3] == board[5])
-                            ||(board[6] != null && board[6] == board[7] && board[6] == board[8])
-                            ||(board[0] != null && board[0] == board[3] && board[0] == board[6])
-                            ||(board[1] != null && board[1] == board[4] && board[1] == board[7])
-                            ||(board[2] != null && board[2] == board[5] && board[2] == board[8])
-                            ||(board[0] != null && board[0] == board[4] && board[0] == board[8])
-                            ||(board[2] != null && board[2] == board[4] && board[2] == board[6]);
+        	if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin()) {
+        		return true;
+        	} else {
+        		return false;
+        	}
+        	
+        }
+        
+        public boolean checkHorizontalWin() {
+        	if (board[0] != null && board[0] == board[1] && board[0] == board[2]) {
+        		return true;
+        	} else if(board[3] != null && board[3] == board[4] && board[3] == board[5]) {
+        		return true;
+        	} else if(board[6] != null && board[6] == board[7] && board[6] == board[8]) {
+        		return true;
+        	} else {
+        		return false;
+        	}
+        }
+        
+        public boolean checkVerticalWin() {
+        	if (board[0] != null && board[0] == board[3] && board[0] == board[6]) {
+        		return true;
+        	} else if(board[1] != null && board[1] == board[4] && board[1] == board[7]) {
+        		return true;
+        	} else if(board[2] != null && board[2] == board[5] && board[2] == board[8]) {
+        		return true;
+        	} else {
+        		return false;
+        	}
+        }
+        
+        public boolean checkDiagonalWin() {
+        	if (board[0] != null && board[0] == board[4] && board[0] == board[8]) {
+        		return true;
+        	} else if (board[2] != null && board[2] == board[4] && board[2] == board[6]) {
+        		return true;
+        	} else {
+        		return false;
+        	}
         }
 
         public synchronized boolean move(PlayerHandler player, int location) { //A player moves based on their assigned Piece (status)
