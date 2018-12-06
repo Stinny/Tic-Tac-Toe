4f778a90be2c2fdb2e8b0174a29824e96297287f
Lucas Raza <razal@wit.edu>
2018-12-06 18:35:32 -0500

    UPDATE

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,5 @@
+import com.sun.corba.se.impl.logging.IORSystemException;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -44,7 +46,7 @@ public class Server {
     }
     }
 
-    class Game{
+    class Game {
 
         private PlayerHandler[] board = {
                 null, null, null,
@@ -53,14 +55,15 @@ public class Server {
         };
         PlayerHandler currentPlayer;
 
-        public boolean isFull(){
-            for(int i = 0; i < board.length; i++) {
-                if(board[i] == null){
+        public boolean isFull() {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i] == null) {
                     return false;
                 }
             }
             return true;
         }
+
         public boolean hasWinner() {
             if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin()) {
                 return true;
@@ -73,9 +76,9 @@ public class Server {
         public boolean checkHorizontalWin() {
             if (board[0] != null && board[0] == board[1] && board[0] == board[2]) {
                 return true;
-            } else if(board[3] != null && board[3] == board[4] && board[3] == board[5]) {
+            } else if (board[3] != null && board[3] == board[4] && board[3] == board[5]) {
                 return true;
-            } else if(board[6] != null && board[6] == board[7] && board[6] == board[8]) {
+            } else if (board[6] != null && board[6] == board[7] && board[6] == board[8]) {
                 return true;
             } else {
                 return false;
@@ -85,9 +88,9 @@ public class Server {
         public boolean checkVerticalWin() {
             if (board[0] != null && board[0] == board[3] && board[0] == board[6]) {
                 return true;
-            } else if(board[1] != null && board[1] == board[4] && board[1] == board[7]) {
+            } else if (board[1] != null && board[1] == board[4] && board[1] == board[7]) {
                 return true;
-            } else if(board[2] != null && board[2] == board[5] && board[2] == board[8]) {
+            } else if (board[2] != null && board[2] == board[5] && board[2] == board[8]) {
                 return true;
             } else {
                 return false;
@@ -106,7 +109,7 @@ public class Server {
 
         public synchronized boolean move(PlayerHandler player, int location) { //A player moves based on their assigned Piece
             System.out.println("Player moved at location " + location);
-            if(board[location]==null){
+            if (board[location] == null) {
                 board[location] = player;
                 currentPlayer = currentPlayer.opponent;
                 currentPlayer.opponentMoved(location);
@@ -114,68 +117,91 @@ public class Server {
             }
             return false;
         }
-        class PlayerHandler extends Thread{
+
+        class PlayerHandler extends Thread {
             char mark;
             private String name;
             private Socket socket;
 
             PlayerHandler opponent;
-            BufferedReader in;
-            PrintWriter out;
+            private BufferedReader in;
+            private PrintWriter out;
 
             public PlayerHandler(Socket socket, char mark) {
                 this.socket = socket;
                 this.mark = mark;
-                try{
+                try {
                     // initialize input and output streams
                     in = new BufferedReader(
                             new InputStreamReader(socket.getInputStream()));
                     out = new PrintWriter(socket.getOutputStream(), true);
                     out.println("WELCOME" + mark);
-                } catch(IOException e){
+                } catch (IOException e) {
 
+                } finally {
                 }
             }
 
-            public void setOpponent(PlayerHandler opponent){
+            public void setOpponent(PlayerHandler opponent) {
                 this.opponent = opponent;
 
             }
 
-            public void opponentMoved(int location){
+            public void opponentMoved(int location) {
                 out.println("OPPONENT_MOVED" + location);
                 out.println(
                         hasWinner() ? "DEFEAT" : isFull() ? "TIE" : "");
             }
 
-            public void run() {
+            public String response() throws Exception {
+                String response = null;
+
                 try {
-                    if (mark == 'X') {
-                        out.println("MESSAGE Your move");
-                    }
-
-                    while (true) {
-                        String command = in.readLine();
-                        if (command != null) {
-
-                            if (command.startsWith("MOVE")) {
-                                int location = Integer.parseInt(command.substring(5));
-                                System.out.println(location);
-                                if (move(this, location)) {
-                                    out.println("VALID_MOVE" + "location");
-                                    out.println(hasWinner() ? "VICTORY"
-                                            : isFull() ? "TIE"
-                                            : "");
-                                } else {
-                                    out.println("MESSAGE ?");
-                                }
-                            }
-                        }
-                    }
-                }catch(IOException e) {
-                    System.out.println("Player died: " + e);
+                    response = in.readLine();
+                    System.out.println(response);
+                    return response;
+                } catch (IOException e) {
+                    System.out.println(e);
                 } finally {
-                    try {socket.close();} catch(IOException e){}
+                    in.close();
+                    socket.close();
+                }
+                return response;
+            }
+
+
+            public void run(){
+                System.out.println("running");
+                try {
+                    while (true) {
+                        String command;
+                        if((command=response())!=null){
+                                System.out.println(command);
+
+                                if (command.startsWith("MOVE")) {
+                                    int location = Integer.parseInt(command.substring(5));
+                                    System.out.println(location);
+                                    if (move(this, location)) {
+                                        out.println("VALID_MOVE" + "location");
+                                        out.println(hasWinner() ? "VICTORY"
+                                                : isFull() ? "TIE"
+                                                : "");
+                                    } else {
+                                        out.println("MESSAGE ?");
+                                    }
+                                }
+
+                        }
+                        System.out.println(command);
+
+
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+
+                }
+                finally{
+                    out.close();
                 }
             }
         }
--- src/main/java/TicTacToeController.java
+++ src/main/java/TicTacToeController.java
@@ -13,7 +13,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.concurrent.Task;
 
 /**
  * Created by lucasraza on 12/1/18.
@@ -30,7 +29,7 @@ public class TicTacToeController implements Initializable {
 
 
     @FXML
-        Button topLeft, topRight, topCenter,
+    Button topLeft, topRight, topCenter,
                 midLeft, midCenter, midRight,
                 botLeft, botMid, botRight;
         @FXML
@@ -39,136 +38,171 @@ public class TicTacToeController implements Initializable {
         public void initialize(URL url, ResourceBundle rb) {
         }
 
-        public void buttonClickHandler(ActionEvent evt) throws Exception{
-                Button clickedButton = (Button) evt.getTarget();
-                evt.getEventType();
+    @FXML
+    public void handleSquareOneClick(ActionEvent event) {
+        out.println("MOVE0");
+        System.out.println("topleft");
+    }
 
-                String button =((Control)evt.getSource()).getId();
-                System.out.println(button);
+    @FXML
+    public void handleSquareTwoClick(ActionEvent event) {
+        out.println("MOVE1");
+        System.out.println("top center");
+    }
 
-                if(button.equals("play")){
-                    clickedButton.setDisable(true);
-                    play();
-                }
-                else if (button.equals("topLeft")) {
-                    out.print("MOVE0");
-                    //do something
-                } else if (button.equals("topRight")) {
-                    //do something
-                    out.print("MOVE2");
-                } else if (button.equals("topCenter")) {
-                    //do something
-                    out.print("MOVE1");
-                } else if (button.equals("midLeft")) {
-                    //do something
-                    out.print("MOVE3");
-                } else if (button.equals("midCenter")) {
-                    //do something
-                    out.print("MOVE4");
-                } else if (button.equals("midRight")) {
-                    //do something
-                    out.print("MOVE5");
-                } else if (button.equals("botLeft")) {
-                    //do something
-                    out.print("MOVE6");
-                } else if (button.equals("botMid")) {
-                    //do something
-                    out.print("MOVE7");
-                } else if (button.equals("botRight")) {
-                    //do something
-                    out.print("MOVE8");
-                }
+    @FXML
+    public void handleSquareThreeClick(ActionEvent event) {
+        out.println("MOVE2");
+        System.out.println("top right");
+    }
+
+    @FXML
+    public void handleSquareFourClick(ActionEvent event) {
+        out.println("MOVE3");
+        System.out.println("mid left");
+    }
+
+    @FXML
+    public void handleSquareFiveClick(ActionEvent event) {
+        out.println("MOVE4");
+        System.out.println("mid center");
+    }
+
+    @FXML
+    public void handleSquareSixClick(ActionEvent event) {
+        out.println("MOVE5");
+        System.out.println("mid right");
+    }
+
+    @FXML
+    public void handleSquareSevenClick(ActionEvent event) {
+        out.println("MOVE6");
+        System.out.println("bottom left");
+    }
+
+    @FXML
+    public void handleSquareEightClick(ActionEvent event) {
+        out.println("MOVE7");
+        System.out.println("bottom center");
+    }
+
+    @FXML
+    public void handleSquareNineClick(ActionEvent event) {
+        out.println("MOVE8");
+        System.out.println("bottom right");
+    }
+
+    @FXML
+    public void handlePlayClick(ActionEvent event){
+        try {
+            play();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+        public boolean buttonClicked(){
+            return true;
         }
         public void play() throws IOException {
-            String response;
+            String welcome;
             try {
                 connectToServer();
-                response = in.readLine();
-                System.out.println("Send to server" + in);
+                welcome = in.readLine();
+                System.out.println(welcome);
 
-                        char mark = response.charAt(7);
+                        char mark = welcome.charAt(7);
                         System.out.println("You are " + mark);
 
                         if (mark == 'X') {
-                            icon = new ImageView(Circle);
-                            opponentIcon = new ImageView(Cross);
-
-                        } else {
                             icon = new ImageView(Cross);
                             opponentIcon = new ImageView(Circle);
+
+                        } else {
+                            icon = new ImageView(Circle);
+                            opponentIcon = new ImageView(Cross);
                         }
                         //frame.setTitle("Tic Tac Toe - Player " + mark);
 
-
-                while (true) {
-                    response = in.readLine();
-                    if (response != null) {
-                        System.out.println(response);
-                        if (response.startsWith("VALID_MOVE")) {
-                            int location = Integer.parseInt(response.substring(5));
-                            if (location == 0) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 1) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 2) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 3) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 4) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 5) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 6) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 7) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 7) {
-                                handleButtonClick(topLeft, icon);
-                            } else if (location == 8) {
-                                handleButtonClick(topLeft, icon);
-                            }
-                        } else if (response.startsWith("OPPONENT_MOVED")) {
-                            int location = Integer.parseInt(response.substring(15));
-                            if (location == 0) {
-                                handleButtonClick(topLeft, opponentIcon);
-                            } else if (location == 1) {
-                                handleButtonClick(topCenter, opponentIcon);
-                            } else if (location == 2) {
-                                handleButtonClick(topRight, opponentIcon);
-                            } else if (location == 3) {
-                                handleButtonClick(midLeft, opponentIcon);
-                            } else if (location == 4) {
-                                handleButtonClick(midCenter, opponentIcon);
-                            } else if (location == 5) {
-                                handleButtonClick(midRight, opponentIcon);
-                            } else if (location == 6) {
-                                handleButtonClick(botLeft, opponentIcon);
-                            } else if (location == 7) {
-                                handleButtonClick(botMid, opponentIcon);
-                            } else if (location == 8) {
-                                handleButtonClick(botRight, opponentIcon);
-                            }
-                        } else if (response.startsWith("VICTORY")) {
-                            //messageLabel.setText("You win");
-                            break;
-                        } else if (response.startsWith("DEFEAT")) {
-                            //messageLabel.setText("You lose");
-                            break;
-                        } else if (response.startsWith("TIE")) {
-                            //messageLabel.setText("You tied");
-                            break;
-                        }
-                    }
-                }
-                out.println("QUIT");
             }
             finally {
+                in.close();
+                out.close();
                 socket.close();
             }
         }
         public void handleButtonClick(Button button, ImageView mark){
             button.setGraphic(mark);
         }
+
+
+    public int playerMove(String response){
+        if (response.startsWith("VALID_MOVE")) {
+            int location = Integer.parseInt(response.substring(5));
+            if (location == 0) {
+                handleButtonClick(topLeft, icon);
+                return 0;
+            } else if (location == 1) {
+                handleButtonClick(topCenter, icon);
+                return 1;
+            } else if (location == 2) {
+                handleButtonClick(topRight, icon);
+                return 2;
+            } else if (location == 3) {
+                handleButtonClick(midLeft, icon);
+                return 3;
+            } else if (location == 4) {
+                handleButtonClick(midCenter, icon);
+                return 4;
+            } else if (location == 5) {
+                handleButtonClick(midRight, icon);
+                return 5;
+            } else if (location == 6) {
+                handleButtonClick(botLeft, icon);
+                return 6;
+            } else if (location == 7) {
+                handleButtonClick(botMid, icon);
+                return 7;
+            } else if (location == 8) {
+                handleButtonClick(botRight, icon);
+                return 8;
+            }
+        }
+        return -1;
+    }
+    public int opponentMoved(String response){
+            if (response.startsWith("OPPONENT_MOVED")) {
+            int location = Integer.parseInt(response.substring(15));
+            if (location == 0) {
+                handleButtonClick(topLeft, opponentIcon);
+            } else if (location == 1) {
+                handleButtonClick(topCenter, opponentIcon);
+            } else if (location == 2) {
+                handleButtonClick(topRight, opponentIcon);
+            } else if (location == 3) {
+                handleButtonClick(midLeft, opponentIcon);
+            } else if (location == 4) {
+                handleButtonClick(midCenter, opponentIcon);
+            } else if (location == 5) {
+                handleButtonClick(midRight, opponentIcon);
+            } else if (location == 6) {
+                handleButtonClick(botLeft, opponentIcon);
+            } else if (location == 7) {
+                handleButtonClick(botMid, opponentIcon);
+            } else if (location == 8) {
+                handleButtonClick(botRight, opponentIcon);
+            }
+        } else if (response.startsWith("VICTORY")) {
+            //messageLabel.setText("You win");
+
+        } else if (response.startsWith("DEFEAT")) {
+            //messageLabel.setText("You lose");
+
+        } else if (response.startsWith("TIE")) {
+            //messageLabel.setText("You tied");
+
+        }
+        return -1;
+    }
 
     public static void connectToServer(){
         try {
